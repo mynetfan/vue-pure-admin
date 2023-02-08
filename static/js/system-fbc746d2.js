c749d368a69adec53531ebf419d16d93fb14bcc9
xiaoxian521 <1923740402@qq.com>
2023-02-08 16:25:49 +0000

    Deploying to gh-pages from @ pure-admin/vue-pure-admin@1a0cd44bb4206e92798215c92aa591c2137e9589 🚀

---
+++ static/js/system-fbc746d2.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-ec0f8f2a.js";const r=t=>e.request("post","/user",{data:t}),o=t=>e.request("post","/role",{data:t}),p=t=>e.request("post","/dept",{data:t});export{o as a,r as b,p as g};
